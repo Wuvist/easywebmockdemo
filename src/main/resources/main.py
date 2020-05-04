33ecbe0f27216c2fd7fff2cdb240db1e2a75d560
Wuvist <wuvist@gmail.com>
2020-05-04 20:52:54 +0800

    Add main.py

---
+++ src/main/resources/main.py
@@ -0,0 +1,14 @@
+from http.server import test, SimpleHTTPRequestHandler
+import argparse, os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--bind', '-b', default='', metavar='ADDRESS',
+                    help='Specify alternate bind address '
+                            '[default: all interfaces]')
+parser.add_argument('port', action='store',
+                    default=8000, type=int,
+                    nargs='?',
+                    help='Specify alternate port [default: 8000]')
+args = parser.parse_args()
+handler_class = SimpleHTTPRequestHandler
+test(HandlerClass=handler_class, port=args.port, bind=args.bind)
